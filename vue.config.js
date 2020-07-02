5fa1b2419b6aec0d0b39c8430671615c3ae98712
jiang <1334417517@qq.com>
2020-07-02 15:03:12 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,84 @@
+
+const path = require('path')
+
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack')
+
+
+module.exports = {
+
+
+    publicPath: '/',
+
+    outputDir: 'dist',
+
+    assetsDir: '',
+
+    indexPath: 'index.html',
+    pages: {
+        index: {
+            // page 的入口
+            entry: 'src/main.js',
+            // 模板来源
+            template: 'public/index.html',
+            // 在 dist/index.html 的输出
+            filename: 'index.html',
+            // 当使用 title 选项时，
+            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'LJCesium',
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+        // 当使用只有入口的字符串格式时，
+        // 模板会被推导为 `public/subpage.html`
+        // 并且如果找不到的话，就回退到 `public/index.html`。
+        // 输出文件名会被推导为 `subpage.html`。
+        // subpage: 'src/subpage/main.js'
+    },
+
+    lintOnSave: process.env.NODE_ENV !== 'production',
+
+    productionSourceMap: false,//去掉生产环境的map
+
+    configureWebpack: {
+        output: {
+            sourcePrefix: ' '
+        },
+        amd: {
+            toUrlUndefined: true
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve('src'),
+            }
+        },
+        plugins: [
+           
+            new webpack.ProvidePlugin({
+                //全局
+                'THREE': require.resolve('three')
+            })
+        ],
+       
+        module: {
+            unknownContextCritical: /^.\/.*$/,
+            unknownContextCritical: false
+        },
+
+    },
+    devServer: {
+        open: process.platform === 'darwin',
+
+        host: '0.0.0.0',
+
+        port: 8082,
+
+        https: false,
+
+        hotOnly: false,
+        // proxy:'/'
+
+    }
+}
